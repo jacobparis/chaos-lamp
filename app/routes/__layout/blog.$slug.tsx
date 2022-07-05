ca161baa6282288aa81e5272986bd6fbf375982d
Jacob Paris <jacob@jacobparis.com>
2022-07-05 17:51:24 +0000

    fix: typescript

--- app/routes/__layout/blog.$slug.tsx
+++ app/routes/__layout/blog.$slug.tsx
@@ -121,7 +121,11 @@ function Excerpt({ children, fade }) {
   )
 }
 
-function Highlight({ children }) {
+function Highlight({ children }: { children?: React.ReactNode }) {
+  if (!children) {
+    return null
+  }
+
   return (
     <span className="-mx-1 bg-yellow-100 py-2 px-1 text-gray-700">
       {" "}
